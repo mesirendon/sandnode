4425368be49a92f438a3ff7c05a1bb92bb79c758
Mesi Rendon <amrendonsa@unal.edu.co>
2019-11-07 10:26:46 -0500

    Base finished
            I've finished the base project

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 #!/usr/bin/env node
 
-const CLI = require('clui');
 const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
-const { exec } = require('child_process');
-
-const Spinner = CLI.Spinner;
-
 
 const inquirer = require('./lib/inquirer');
+const packager = require('./lib/package.js');
+const files = require('./lib/files.js');
 
 clear();
 console.log(
@@ -19,45 +16,11 @@ console.log(
 );
 
 const run = async () => {
-  const packs = [
-    "@babel/core",
-    "@babel/cli",
-    "@babel/preset-env",
-    "@babel/node",
-    "@babel/preset-stage-3",
-    "nodemon",
-    "babel-register",
-  ];
   const packInit = await inquirer.promptProjectQuestions();
-  const status = new Spinner('Setting packages');
-  status.start();
-  exec('pwd', (err, stdout, stderr) => {
-    if(err) {
-      chalk.red(console.error(err));
-    } else {
-      console.log(stdout);
-      console.log(stderr);
-    }
-  });
-  //if(packInit.packer === 'yarn') {
-    //exec(`yarn config set init-author-name ${ packInit.author }`);
-    //exec(`yarn config set init-author-email ${ packInit.email }`);
-    //exec(`yarn config set init-version ${ packInit.version }`);
-    //exec(`yarn config set init-license ${ packInit.license }`);
-    //exec('yarn init -y');
-    //exec(`yarn add -D ${ packs.join(' ') }`, (err, a, b) => {
-      //if(err) console.error(err);
-      //else {
-        //console.log(a);
-        //console.log(b);
-      //}
-    //});
-  //} else {
-    //exec('npm init -y');
-    //exec(`npm i --save-dev ${ packs.join(' ') }`);
-  //}
-  status.stop();
-
+  console.log(chalk.green('Package initialization...'));
+  const result = await packager.installPacks(packInit);
+  console.log(chalk.green('Scaffolding directory...'));
+  files.copyFiles();
 }
 
 run();
